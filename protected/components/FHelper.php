22441a3f11590feda99033366337f5c81a28b8e5
elisgen <imperia1991@gmail.com>
2013-06-18 19:56:32 +0300

    Вывод попутных грузов

--- protected/components/FHelper.php
+++ protected/components/FHelper.php
@@ -1,57 +1,1 @@
-<?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- * Description of FHelper
- *
- * @author Gennadiy
- */
-class FHelper
-{
-
-	const EARTH_RADIUS = 6372795;
-
-	/*
-	 * Расстояние между двумя точками
-	 * $φA, $λA - широта, долгота 1-й точки,
-	 * $φB, $λB - широта, долгота 2-й точки
-	 * Написано по мотивам http://gis-lab.info/qa/great-circles.html
-	 * Михаил Кобзарев <kobzarev@inforos.ru>
-	 *
-	 */
-
-	public static function distance($φA, $λA, $φB, $λB)
-	{
-
-		// перевести координаты в радианы
-		$lat1 = $φA * M_PI / 180;
-		$lat2 = $φB * M_PI / 180;
-		$long1 = $λA * M_PI / 180;
-		$long2 = $λB * M_PI / 180;
-
-		// косинусы и синусы широт и разницы долгот
-		$cl1 = cos($lat1);
-		$cl2 = cos($lat2);
-		$sl1 = sin($lat1);
-		$sl2 = sin($lat2);
-		$delta = $long2 - $long1;
-		$cdelta = cos($delta);
-		$sdelta = sin($delta);
-
-		// вычисления длины большого круга
-		$y = sqrt(pow($cl2 * $sdelta, 2) + pow($cl1 * $sl2 - $sl1 * $cl2 * $cdelta, 2));
-		$x = $sl1 * $sl2 + $cl1 * $cl2 * $cdelta;
-
-		//
-		$ad = atan2($y, $x);
-		$dist = $ad * self::EARTH_RADIUS;
-
-		return ceil($dist / 1000);
-	}
-
-}
-?>
+<?php/* * To change this template, choose Tools | Templates * and open the template in the editor. *//** * Description of FHelper * * @author Gennadiy */class FHelper {	const EARTH_RADIUS = 6372795;	/*	 * Расстояние между двумя точками	 * $φA, $λA - широта, долгота 1-й точки,	 * $φB, $λB - широта, долгота 2-й точки	 * Написано по мотивам http://gis-lab.info/qa/great-circles.html	 * Михаил Кобзарев <kobzarev@inforos.ru>	 *	 */	public static function distance($φA, $λA, $φB, $λB) {		// перевести координаты в радианы		$lat1 = $φA * M_PI / 180;		$lat2 = $φB * M_PI / 180;		$long1 = $λA * M_PI / 180;		$long2 = $λB * M_PI / 180;		// косинусы и синусы широт и разницы долгот		$cl1 = cos($lat1);		$cl2 = cos($lat2);		$sl1 = sin($lat1);		$sl2 = sin($lat2);		$delta = $long2 - $long1;		$cdelta = cos($delta);		$sdelta = sin($delta);		// вычисления длины большого круга		$y = sqrt(pow($cl2 * $sdelta, 2) + pow($cl1 * $sl2 - $sl1 * $cl2 * $cdelta, 2));		$x = $sl1 * $sl2 + $cl1 * $cl2 * $cdelta;		//		$ad = atan2($y, $x);		$dist = $ad * self::EARTH_RADIUS;		return ceil($dist / 1000);	}}?>
