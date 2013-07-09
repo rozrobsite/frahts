2da84adc0118031d610983ed070507650c4634a5
nikotin <nikotin69@ukr.net>
2013-07-09 18:04:27 +0300

    На страничку юзера добавлены грузы и транспорт

--- protected/views/user/view.php
+++ protected/views/user/view.php
@@ -7,7 +7,7 @@
 <?php
 
 Yii::app()->clientScript->registerScriptFile('http://api-maps.yandex.ru/2.0/?load=package.full&lang=ru-RU');
-Yii::app()->clientScript->registerScriptFile('/js/files/userMap.js'); //включить карту
+//Yii::app()->clientScript->registerScriptFile('/js/files/userMap.js'); //включить карту
 
 $this->pageTitle = Yii::app()->name . ' - Данные о пользователе "' . $model->profiles->last_name . ' ' . $model->profiles->first_name . ' ' . $model->profiles->middle_name . '"';
 $this->breadcrumbs = array(
@@ -60,9 +60,7 @@ $this->breadcrumbs = array(
 
                 <div class="inContainer">
                     <div class="inFrom" >                        					
-						<h5><?php echo $model->profiles->last_name . ' ' . $model->profiles->first_name . ' ' . $model->profiles->middle_name; ?>
-						<span class="avatar" style="width:50px;height:50px; border:1px solid; border-color:black; display:inline-block; position:relative; float:right; top:0px;">Аватар?</span>
-						</h5>
+						<h5><?php echo $model->profiles->last_name . ' ' . $model->profiles->first_name . ' ' . $model->profiles->middle_name; ?></h5>
 					
 						<span><strong><?php echo $model->profiles->userType->name_ru; ?></strong></span>
 						
@@ -105,13 +103,30 @@ $this->breadcrumbs = array(
 				<div class="inContainer">
 					<div class="widget grid6">       
 						<ul class="tabs">
-							<li class="activeTab" style=""><a href="#tabb1">Отзывы</a></li>
-							<li class=""><a href="#tabb2">Грузы</a></li>
-							<li class=""><a href="#tabb3">Транспорт</a></li>
+							<?php 
+								switch ($model->profiles->userType->id) {
+									case 1:
+										$printVehicles = true;
+										$printGoods = false;
+										break;
+									case 2:
+										$printVehicles = false;
+										$printGoods = true;
+										break;
+									default:
+										$printVehicles = true;
+										$printGoods = true;
+								}							
+							?>
+						
+						
+							<li class="activeTab" style=""><a href="#tab_comments">Отзывы</a></li>
+							<?php  if ($printGoods): ?> <li class=""><a href="#tab_goods">Грузы</a></li> <?php endif; ?>
+							<?php  if ($printVehicles): ?> <li class=""><a href="#tab_vehicles">Транспорт</a></li> <?php endif; ?>
 						</ul>
 						
 						<div class="tab_container">
-							<div id="tabb1" class="tab_content" style="display: block;">
+							<div id="tab_comments" class="tab_content" style="display: block;">
 									<!-- Enter messages field -->
 									<div class="enterMessage">
 										<input type="text" name="enterMessage" placeholder="Enter your message..." />
@@ -126,7 +141,7 @@ $this->breadcrumbs = array(
 										<ul class="messagesTwo">
 											<li class="by_user">
 												<a href="#" title=""><img src="images/live/face1.png" alt="" /></a>
-												<div class="rating" style="display:inline-block; position:relative; left:10px; top:50px;">Оценка</div>
+												<!--<div class="rating" style="display:inline-block; position:relative; left:10px; top:50px;">Оценка</div>-->
 												<div class="messageArea">
 													<div class="infoRow">
 														<span class="name"><strong>Jonathan</strong> says:</span>
@@ -158,12 +173,179 @@ $this->breadcrumbs = array(
 
 		
 							</div>
+<!-- ********************************************* -->							
+							<?php  if ($printGoods): ?>
+							<div id="tab_goods" class="tab_content" style="display: none;"> 
 
-							<div id="tabb2" class="tab_content" style="display: none;"> Грузы							
+								<?php  if ($model->goods && count($model->goods) > 0): ?>
+									<table width="100%" border="0" cellspacing="0" cellpadding="0" class="tLight" style="text-align: center;">
+										<thead>
+											<tr>
+												<td width="40%">Маршрут</td>
+												<td width="15%">Дата доставки</td>
+												<td width="25%">Груз</td>
+												<td width="20%">Дата добавления</td>
+											</tr>
+										</thead>
+										<tbody>
+											<?php foreach($model->goods as $oneGood): ?>
+												<?php if (!$oneGood->is_deleted && $oneGood->date_to >= time() && $oneGood->id != $model->id): ?>
+													<tr>
+														<td>
+															<span>
+																<?php echo $oneGood->countryFrom->name_ru . ' - ' .  $oneGood->countryTo->name_ru?>
+															</span><br/>
+															<span>
+																<?php echo $oneGood->regionFrom->name_ru . ' - ' .  $oneGood->regionTo->name_ru?>
+															</span><br/>
+															<span>
+																<?php echo $oneGood->cityFrom->name_ru . ' - ' .  $oneGood->cityTo->name_ru?>
+															</span>
+															<br/>
+															<span>
+																<strong>&asymp;&nbsp;<?php echo ((int) FHelper::distance($oneGood->cityFrom->latitude, $oneGood->cityFrom->longitude, $oneGood->cityTo->latitude, $oneGood->cityTo->longitude) + 10) ?> км</strong>
+															</span>
+														</td>
+														<td>
+															с
+															<?php echo Yii::app()->dateFormatter->format('dd.MM.yyyy', $oneGood->date_from); ?><br/>
+															по
+															<?php echo Yii::app()->dateFormatter->format('dd.MM.yyyy', $oneGood->date_to); ?>
+														</td>
+														<td>
+															<a href="/goods/view/<?php echo $oneGood->slug ?>" class="tipS" title="Перейти на страницу груза">
+																<?php echo $oneGood->name ?><br/>
+																<?php
+																$weight = 0;
+																if (isset($oneGood->weight_exact_value) && $oneGood->weight_exact_value)
+																{
+																	$weight = $oneGood->weight_exact_value;
+																}
+																elseif (isset($oneGood->weight_to) && $oneGood->weight_to)
+																{
+																	$weight = $oneGood->weight_to;
+																}
+																?>
+																Вес до: <?php echo $weight ?> т.<br/>
+																<?php
+																$capacity = 0;
+																if (isset($oneGood->capacity_exact_value) && $oneGood->capacity_exact_value)
+																{
+																	$capacity = $oneGood->capacity_exact_value;
+																}
+																elseif (isset($oneGood->capacity_to) && $oneGood->capacity_to)
+																{
+																	$capacity = $oneGood->capacity_to;
+																}
+																?>
+																Объем до: <?php echo $capacity ?> м&sup3;<br/>
+															</a>
+														</td>
+														<td>
+															<?php echo Yii::app()->dateFormatter->format('dd.MM.yyyy', ($oneGood->updated_at ? $oneGood->updated_at : $oneGood->created_at)); ?><br/>
+															<?php echo Yii::app()->dateFormatter->format('HH:mm', ($oneGood->updated_at ? $oneGood->updated_at : $oneGood->created_at)); ?>
+														</td>
+													</tr>
+												<?php endif; ?>
+											<?php endforeach; ?>
+										</tbody>
+									</table>
+								<?php else: ?>
+									<div class="fluid" style="text-align: center;margin-top: 50px;">
+										<label style="font-weight: bold; font-size: 16px;">
+											Нет грузов.
+										</label>
+									</div>
+								
+								<?php endif; ?>
+						
 							</div>
+							<?php endif; ?>
+<!-- ********************************************* -->	
+<!-- ============================================= -->	
+							<?php  if ($printVehicles): ?>
+							<div id="tab_vehicles" class="tab_content" style="display: none;"> 
 
-							<div id="tabb3" class="tab_content" style="display: none;"> Транспорт							
-							</div>							
+
+								<?php if ($model->vehicles && count($model->vehicles) > 1): ?>
+									<table width="100%" border="0" cellspacing="0" cellpadding="0" class="tLight" style="text-align: center;">
+										<thead>
+											<tr>
+												<td width="10%">Фото</td>
+												<td width="25%">Название</td>
+												<td width="40%">Расположение</td>
+												<td width="25%">Характеристики</td>
+											</tr>
+										</thead>
+										<tbody>
+											<?php foreach($model->vehicles as $vehicle): ?>
+												<?php if (!$vehicle->is_deleted && $vehicle->id != $model->id): ?>
+													<tr>
+														<td>
+															<a href="/vehicle/view/<?php echo $vehicle->slug; ?>" class="tipS" title="Перейти на страницу транспортного средства" style="display: block;">
+																<?php $image = isset($vehicle->photos[0])
+																	? '/' . Yii::app()->params['files']['photos'] . '/' . $vehicle->photos[0]->size_middle
+																	: '/images/nophoto.jpg' ?>
+																<img src="<?php echo $image; ?>" alt="" />
+															</a>
+														</td>
+														<td>
+															<a href="/vehicle/view/<?php echo $vehicle->slug; ?>" class="tipS" title="Перейти на страницу транспортного средства">
+																<?php echo ucfirst($vehicle->vehicleType->name_ru) . " " . $vehicle->marka->name . " " . $vehicle->modeli->name ?>
+															</a><br/>
+															<strong>Добавлен:</strong><br/>
+															<span>
+																<?php echo Yii::app()->dateFormatter->format('dd.MM.yyyy', $vehicle->updated_at); ?>&nbsp;
+																<?php echo Yii::app()->dateFormatter->format('HH:mm', $vehicle->updated_at); ?>
+															</span>
+														</td>
+														<td>
+															<?php if ($vehicle->city_id): ?>
+															<span>
+																<?php echo $vehicle->countries->name_ru . ' - ' . (!empty($vehicle->countriesTo->name_ru) ? $vehicle->countriesTo->name_ru : 'Любая'); ?>
+															</span>
+															<span>
+																<?php echo $vehicle->regions->name_ru . ' - ' . (!empty($vehicle->regionsTo->name_ru) ? $vehicle->regionsTo->name_ru : 'Любая'); ?>
+															</span>
+															<span>
+																<?php echo $vehicle->cities->name_ru . ' - ' . (!empty($vehicle->citiesTo->name_ru) ? $vehicle->citiesTo->name_ru : 'Любой'); ?>
+															</span>
+															<span>
+																<?php if ($vehicle->date_from && $vehicle->date_to): ?>
+																	c <?php echo Yii::app()->dateFormatter->format('dd.MM.yyyy', $vehicle->date_from); ?> по <?php echo Yii::app()->dateFormatter->format('dd.MM.yyyy', $vehicle->date_to); ?>
+																<?php endif; ?>
+															</span>
+															<?php endif; ?>
+														</td>
+														<td>
+															<span><strong>Тип кузова: </strong>
+																<?php echo $vehicle->bodyType->name_ru ?>
+															</span>
+															<span><strong>Вид загрузки: </strong>
+																<?php echo $vehicle->shipmentsNames ?>
+															</span>
+															<span><strong>Объем кузова: </strong>
+																<?php echo $vehicle->body_capacity ?> м&sup3;
+															</span>
+														</td>
+													</tr>
+												<?php endif; ?>
+											<?php endforeach; ?>
+										</tbody>
+									</table>
+								<?php else: ?>
+										<div class="fluid" style="text-align: center;margin-top: 50px;">
+											<label style="font-weight: bold; font-size: 16px;">
+												Нет транспорта.
+											</label>
+										</div>
+							
+								<?php endif; ?>
+
+
+							</div>		
+							<?php endif; ?>
+<!-- ============================================= -->								
 						</div>	
 						<div class="clear"></div>		 
 					</div>	
