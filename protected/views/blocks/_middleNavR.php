0071f73b8060783baf2bea1fe9a2f960cee974bb
elisgen <imperia1991@gmail.com>
2013-07-18 20:05:22 +0300

    Поиск без диспетчеров

--- protected/views/blocks/_middleNavR.php
+++ protected/views/blocks/_middleNavR.php
@@ -1,8 +1,8 @@
-<div class="nNote nInformation">
+<!--<div class="nNote nInformation">
 	<p>
 		<span class="greenBack">Здесь может быть баннер или два, или три (больше не надо)</span><br/>или<br/>
 		Новинки сайта: теперь на сайте есть возможность добавлять пользователей в партнеры!<br/>
 		Спасибо что Вы с нами.<br/>
 		<span class="greyBack" style="font-style: italic; font-size: 10px;">(Кликните на эту панель чтобы скрыть ее)</span>
 	</p>
-</div>
+</div>-->
