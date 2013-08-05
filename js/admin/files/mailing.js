982aea202c8e07bd037251a12c6ddbad96997594
elisgen <imperia1991@gmail.com>
2013-08-05 19:30:50 +0300

    Массовая рассылка

--- js/admin/files/mailing.js
+++ js/admin/files/mailing.js
@@ -1,12 +1,12 @@
-var mailing = {
-	send: function() {
-		$('#mailing-add').on('submit', function(e){
-			alert(111);
-
-			return false;
-		});
-	}
-}
-$(document).ready(function(){
-	mailing.send();
-});
+//var mailing = {
+//	send: function() {
+//		$('#mailing-add').on('submit', function(e){
+//			alert(111);
+//
+//			return false;
+//		});
+//	}
+//}
+//$(document).ready(function(){
+//	mailing.send();
+//});
